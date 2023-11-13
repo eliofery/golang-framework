6f8b2c866ba801614eaf0dd985732e209a34c44b
Sergey Erofeev <erofsey@yandex.kz>
2023-11-13 10:20:16 +0600

    Провел рефакторинг и добавил middlewares

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
+	mw "github.com/eliofery/golang-image/internal/app/http/middleware"
 	"github.com/eliofery/golang-image/pkg/config"
 	"github.com/eliofery/golang-image/pkg/cookie"
 	"github.com/eliofery/golang-image/pkg/database"
 	"github.com/eliofery/golang-image/pkg/database/postgres"
+	"github.com/eliofery/golang-image/pkg/email"
 	"github.com/eliofery/golang-image/pkg/logging"
 	"github.com/eliofery/golang-image/pkg/rand"
 	"github.com/eliofery/golang-image/pkg/router"
 	"github.com/eliofery/golang-image/pkg/tpl"
-	"github.com/gorilla/csrf"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/pressly/goose/v3"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -25,79 +26,39 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Создание логирования
-	logger := logging.New("prod")
-
-	//// Подключение к БД SQLite
-	//driver := sqlite.New()
-	//db, err := database.Init(driver)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//// Миграция БД SQLite
-	//if err = database.MigrateFS(db, goose.DialectSQLite3); err != nil {
-	//	log.Fatal(err)
-	//}
+	// Подключение логирования
+	logger := logging.New()
 
 	// Подключение к БД Postgres
-	driver, err := postgres.New()
+	db, err := database.Init(postgres.New())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
-
-	db, err := database.Init(driver)
-	if err != nil {
-		log.Fatal(err)
-	}
+	defer func(db *sql.DB) {
+		err = db.Close()
+		if err != nil {
+			logger.Error(err.Error())
+		}
+	}(db)
 
 	// Миграция БД Postgres
 	if err = database.MigrateFS(db, goose.DialectPostgres); err != nil {
-		log.Fatal(err)
+		logger.Error(err.Error())
 	}
-
-	//// Отправка почты
-	//emailService, err := email.New()
-	//if err != nil {
-	//    logger.Info("не удалось создать подключение к smtp", err)
-	//}
-	//
-	//mail := email.Email{
-	//    From: "support@example.kz",
-	//    To: "guest@example.kz",
-	//    Subject: "Регистрация на сайте",
-	//    Plaintext: `
-	//        Регистрация прошла успешно.
-	//
-	//        Добро пожаловать к нам на сайт, рады вас видеть.
-	//    `,
-	//    HTML: `
-	//        <h1>Регистрация прошла успешно.</h1>
-	//
-	//        <p>Добро пожаловать к нам на сайт, рады вас видеть.</p>
-	//    `,
-	//}
-	//err = emailService.Send(mail)
-	//if err != nil {
-	//    logger.Info("не удалось отправить почту", err)
-	//}
-
-	// Получение токена
-	token, err := rand.SessionToken()
-	if err != nil {
-		logger.Info("не удалось получить токен", err)
-	}
-	fmt.Println(token)
 
 	// Создание роутера
 	route := router.New()
 
 	// Пользовательский Middleware
-	csrfSecure, _ := strconv.ParseBool(os.Getenv("CSRF_SECURE"))
-	route.Use(csrf.Protect([]byte(os.Getenv("CSRF_KEY")), csrf.Secure(csrfSecure)))
-	route.Use(Middleware)
+	route.Use(middleware.RequestID)
+	route.Use(middleware.RealIP)
+	route.Use(middleware.Logger)
+	route.Use(middleware.Recoverer)
+	route.Use(middleware.URLFormat)
+	route.Use(mw.Bind(logger, db))
+	route.Use(mw.Csrf)
 
-	// Ресурсы
+	// Подключение ресурсов
 	tpl.AssetsFsInit(route.Mux)
 
 	// Тестовый роут
@@ -106,55 +67,95 @@ func main() {
 
 	// Запуск сервера
 	logger.Info("Сервер запущен: http://localhost:8080")
-	http.ListenAndServe(":8080", route.ServeHTTP())
+	err = http.ListenAndServe(":8080", route.ServeHTTP())
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
 
 func index(ctx router.Ctx) error {
-	val := ctx.Value("test")
-	str := val.(string)
+	w := router.ResponseWriter(ctx)
+	r := router.Request(ctx)
+	l := logging.Logging(ctx)
+	db := database.CtxDatabase(ctx)
+
+	var value string
+	res := db.QueryRow("SELECT value FROM info WHERE id = 2")
+	err := res.Scan(&value)
+	if err != nil {
+		return err
+	}
+	fmt.Println(value)
+
+	// Получение токена
+	token, err := rand.SessionToken()
+	if err != nil {
+		l.Info("не удалось получить токен", err)
+	}
 
 	data := struct {
 		Test string
 	}{
-		Test: str,
+		Test: token,
 	}
 
-	w := router.ResponseWriter(ctx)
-	r := router.Request(ctx)
-
 	// Чтение куки
-	res, _ := cookie.Get(r, "test")
-	fmt.Println(res)
+	ck, _ := cookie.Get(r, "test")
+	if ck != "" {
+		fmt.Println(res)
+	} else {
+		fmt.Println("нет куки")
+	}
 
 	// Удаление куки
 	cookie.Delete(w, "test")
 
-	err := tpl.Render(ctx, "home", data)
-
-	return err
+	return tpl.Render(ctx, "home", data)
 }
 
 func post(ctx router.Ctx) error {
 	w := router.ResponseWriter(ctx)
 	r := router.Request(ctx)
+	l := logging.Logging(ctx)
 
 	// Добавление куки
 	cookie.Set(w, "test", "2685723587236582730")
 
 	value := r.FormValue("test")
 
-	w.Write([]byte(value))
+	// Отправка почты
+	emailService, err := email.New()
+	if err != nil {
+		l.Info("не удалось создать подключение к smtp", err)
+	}
+	_ = emailService
+
+	mail := email.Email{
+		From:    "support@example.kz",
+		To:      "guest@example.kz",
+		Subject: "Регистрация на сайте",
+		Plaintext: `
+	       Регистрация прошла успешно.
+
+	       Добро пожаловать к нам на сайт, рады вас видеть.
+	   `,
+		HTML: `
+	       <h1>Регистрация прошла успешно.</h1>
+
+	       <p>Добро пожаловать к нам на сайт, рады вас видеть.</p>
+	   `,
+	}
+	_ = mail
+	//err = emailService.Send(mail)
+	//if err != nil {
+	//   l.Info("не удалось отправить почту", err)
+	//}
+
+	_, err = w.Write([]byte(value))
+	if err != nil {
+		return err
+	}
 
 	return nil
-}
-
-func Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("first middleware")
-
-		ctx := context.WithValue(r.Context(), "test", "Тест передачи контекста через middleware прошел успешно")
-		r = r.WithContext(ctx)
-
-		next.ServeHTTP(w, r)
-	})
 }
--- go.mod
+++ go.mod
@@ -4,15 +4,16 @@ go 1.21.4
 
 require (
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-mail/mail/v2 v2.3.0
 	github.com/gorilla/csrf v1.7.2
 	github.com/jackc/pgx/v5 v5.5.1
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.19
 	github.com/pressly/goose/v3 v3.16.0
+	golang.org/x/net v0.18.0
 )
 
 require (
-	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -23,4 +24,5 @@ require (
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 )
