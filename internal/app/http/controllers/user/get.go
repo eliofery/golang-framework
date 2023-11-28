f0777e9d63934fa46667a8f7ebbfefec3bfcba44
Sergey Erofeev <erofsey@yandex.kz>
2023-11-28 11:54:17 +0600

    Добавил отображение данных на странице пользователя

--- internal/app/http/controllers/user/get.go
+++ internal/app/http/controllers/user/get.go
@@ -1,12 +1,17 @@
 package user
 
 import (
+	"github.com/eliofery/golang-image/internal/app/models/user"
 	"github.com/eliofery/golang-image/pkg/router"
 	"github.com/eliofery/golang-image/pkg/tpl"
 )
 
 func Index(ctx router.Ctx) error {
-	return tpl.Render(ctx, "user/index", tpl.Data{})
+	userData := user.CtxUser(ctx)
+
+	return tpl.Render(ctx, "user/index", tpl.Data{
+		Data: userData,
+	})
 }
 
 func SignUp(ctx router.Ctx) error {
